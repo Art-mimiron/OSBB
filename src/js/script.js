f8ab783fd821f61a61f6e6b7378c8af8b68731b1
Artem <artmimiron@gmail.com>
2020-09-26 01:23:01 +0300

    +feed \ +footer_part

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,21 @@ document.querySelector(".dot_prev").addEventListener('click', function () {
 document.querySelector(".dot_next").addEventListener('click', function () {
   slider.goTo('next');
 });
+
+const slider2 = tns({
+    container: '.feed__carousel',
+    items: 2,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    autoHeight: true,
+    gutter: 50,
+    navPosition: 'bottom'
+  });
+
+document.querySelector(".dot_prev2").addEventListener('click', function () {
+  slider2.goTo('prev');
+});
+document.querySelector(".dot_next2").addEventListener('click', function () {
+  slider2.goTo('next');
+});
